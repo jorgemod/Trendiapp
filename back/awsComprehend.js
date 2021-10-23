88db3a2505ff46a9af4ab6d869423674abb22b0e
AlexanderBlaise <alexander.rangel@alumno.buap.mx>
2021-10-23 13:43:42 -0500

    🔨Export method feeling

--- back/awsComprehend.js
+++ back/awsComprehend.js
@@ -1,20 +1,25 @@
-const path = require('path')
-
 var AWS = require('aws-sdk');
+const path = require('path')
 var direccion = path.join(__dirname, "awsConfig.json")
 AWS.config.loadFromPath(direccion);
 
-var comprehend = new AWS.Comprehend({
-    apiVersion: '2017-11-27',
-    region: 'us-east-1'
-});
 
-var params = {
-    LanguageCode: 'es',
-    Text: 'texto'
-};
+const sentimientos = (text) => {
+    var comprehend = new AWS.Comprehend({
+        apiVersion: '2017-11-27',
+        region: 'us-east-1'
+    });
+    var params = {
+        LanguageCode: 'es',
+        Text: text
+    };
+    
+    comprehend.detectSentiment(params, function(err, data){
+        if(err) 
+            console.log(err, err.stack);
+        else 
+            console.log(data);
+    });
+}
 
-comprehend.detectSentiment(params, function(err, data){
-    if(err) console.log(err, err.stack);
-    else console.log(data);
-});
+module.exports = sentimientos;
