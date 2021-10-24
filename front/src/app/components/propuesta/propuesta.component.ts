db2e1d405657f400c9eba17a8a5ab4b2f7f19c2f
jorge <coqueju97@gmail.com>
2021-10-24 05:08:01 -0500

    routes navigate active

--- front/src/app/components/propuesta/propuesta.component.ts
+++ front/src/app/components/propuesta/propuesta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { DatabankService } from 'src/app/databank.service';
 
 @Component({
@@ -7,12 +8,24 @@ import { DatabankService } from 'src/app/databank.service';
   styleUrls: ['./propuesta.component.css']
 })
 export class PropuestaComponent implements OnInit {
-  
-  constructor(private prueba: DatabankService) { 
+  pestania; 
+  constructor(private prueba: DatabankService, private router: ActivatedRoute) { 
+    
+    this.router.params.subscribe(params => {
+      console.log(params['id']);
+      if (params['id'] ==1 ){
+        this.pestania = "Crecer en Clientes"
+      }
+      else{
+        
+        this.pestania = "Excelencia Operativa"
+      }
+    });
     this.prueba.data().subscribe( paises=>{
       
       console.log(paises);
     })
+
   }
 
   ngOnInit(): void {
