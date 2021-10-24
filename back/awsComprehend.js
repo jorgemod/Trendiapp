5be11cfb1aec5895269410eb466722210b819cad
vanyapb <ivan.perez@lisder.buap.mx>
2021-10-24 00:32:31 -0500

    UÂpdate AWS detect sentiment to batch request

--- back/awsComprehend.js
+++ back/awsComprehend.js
@@ -1,25 +1,66 @@
 const path = require('path')
-
+const fetch = require('node-fetch');
 var AWS = require('aws-sdk');
+const { ComprehendClient, BatchDetectSentimentCommand, BatchDetectSentimentRequest } = require("@aws-sdk/client-comprehend");
 var direccion = path.join(__dirname, "awsConfig.json")
 AWS.config.loadFromPath(direccion);
+
+var config = ({
+    "apiVersion": "2017-11-27",
+    "accessKeyId": "AKIA54WBYCE4NLRWHEQM",
+    "secretAccessKey": "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",
+    "region": "us-east-1"
+  });
+
+const client = new ComprehendClient({
+    region: 'us-east-1',
+    credentials: {
+        accessKeyId: "AKIA54WBYCE4NLRWHEQM",
+        secretAccessKey: "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"
+    }
+});
+
 
 var comprehend = new AWS.Comprehend({
     apiVersion: '2017-11-27',
     region: 'us-east-1'
 });
 
-const sentimientos = ()=>{
-    
+const fetchData = async (params) => {
+    try {
+        fetchObj = await comprehend.detectSentiment(params);
+        obj = fetchObj.send();
+        console.log("fetchObj", obj);
+        // await comprehend.detectSentiment(params, function(err, data){
+        //     if(err) console.log(err, err.stack);
+        //     else {
+        //         console.log("data.Sentiment", data.Sentiment);
+        //         return data.Sentiment;
+        //     } 
+        // });
+    } catch(err) {
+        console.log("err.message", err.message);
+    }
+}
+
+async function sentimientos (data) {
+    const textList = data.map(twit => {return twit.text;})
+    let response;
+
+    console.log("textList", textList)
     var params = {
-        LanguageCode: 'es',
-        Text: 'chinga tu madre alexander'
+        LanguageCode: "es",//en | es | fr | de | it | pt | ar | hi | ja | ko | zh | zh-TW, /* required */
+        TextList: textList
     };
     
-    comprehend.detectSentiment(params, function(err, data){
-        if(err) console.log(err, err.stack);
-        else console.log(data);
-    });
+    try {
+        const command = new BatchDetectSentimentCommand(params);
+        response = await client.send(command);
+        console.log("response", response);
+    } catch(err) {
+        console.log("err.message", err);
+    }
+    return response;
 }
 
 
--- back/controllers/twitter.js
+++ back/controllers/twitter.js
@@ -1,5 +1,6 @@
-var Twit = require('twit')
-const sentimientos = require('../awsComprehend');
+var Twit = require('twit');
+var sentimientos = require('../awsComprehend.js');
+
 var T = new Twit({
   consumer_key:         'BExYU0PjOCDom40t2324RxDCt',
   consumer_secret:      '493E6O061c4a1NPeHC10gDNFMSQc4ArHlER5ziVh2WrPZiWlcY',
@@ -8,14 +9,27 @@ var T = new Twit({
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
+let sentimiento;
+
+const fetchData = async (res, data) => {
+  try {
+    sentimiento = await sentimientos(data.statuses[0].text);
+    console.log("sentimiento", sentimiento);
+    return sentimiento;
+  } catch (err) {
+    console.log("err.message", err.message);
+  }
+}
 
 exports.searchTwit = (req, res) => {
-    T.get('search/tweets', { q: req.query.word + ' since:2020-03-01', value: 'place_country: MX', count: 10 }, function(err, data, response) {
-        // console.log("req.params", req.query.word);
-        // console.log("data", data);
-        sentimientos();
-        res.send(data.statuses.map((twit) => {
-            return {"twit": twit.text, "place": twit.place}
-        }));
-      });
+  T.get('search/tweets', { q: req.query.word + ' since:2020-03-01', value: 'place_country: MX', count: 10 }, async function(err, data, response) {
+    // console.log("req.params", req.query.word);
+    // console.log("data", data);
+
+    const result = await sentimientos(data.statuses);
+    console.log("result", result);
+    res.send(data.statuses.map( (twit) => {
+      return {"twit": twit.text, "place": twit.place, "sentimiento": "happy"}            
+    }));
+  });
 }
